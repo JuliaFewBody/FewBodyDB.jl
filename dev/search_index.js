6b939d2432f3c746fa302ed125e0e7adc476a6e2
Documenter.jl <documenter@juliadocs.github.io>
2025-09-23 12:25:15 +0000

    build based on 5c9ff44

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#FewBodyDB.jl","page":"Home","title":"FewBodyDB.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for FewBodyDB.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
